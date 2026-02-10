6bf2928cfb8c4de4f28b3cbbc42b23b115c4ea14
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-02-10 09:42:49 +0100

    Add TypeScript configuration and update Jest settings for improved testing

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,23 @@
 export default {
+    testEnvironment: 'node',
+
     setupFiles: ['<rootDir>/spec/jest.env.js'],
-    testMatch: ['**/spec/**/*.spec.js'],
+
+    testMatch: ['**/spec/**/*.spec.{js,ts}'],
+
+    extensionsToTreatAsEsm: ['.ts'],
+
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.json', useESM: true },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+
     collectCoverage: true,
     coverageReporters: ['html', 'text', 'text-summary'],
     coverageDirectory: 'coverage',
-    testPathIgnorePatterns: ['/e2e/', '/tests/'],
+
+    testPathIgnorePatterns: ['/e2e/', '/tests/', '/dist/'],
 };
