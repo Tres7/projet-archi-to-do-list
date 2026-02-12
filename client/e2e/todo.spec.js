cf0368d88d24c0259b6aa722a0413033b249c4e8
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-02-12 08:47:51 +0100

    change e2e BASE_URL

--- client/e2e/todo.spec.js
+++ client/e2e/todo.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const BASE_URL = 'http://localhost:5174';
+const BASE_URL = 'http://localhost:5173';
 
 async function openApp(page) {
     await page.goto(`${BASE_URL}/`);
