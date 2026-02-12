4fb31511c20271a6cecc87294635bd9cc5825604
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-02-12 08:19:50 +0100

    Extracting AddItemForm component and refactor App to use it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Row, Col, Form, InputGroup, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import ItemDisplay from './ItemDisplay';
+import AddItemForm from './AddItemForm';
 
 export default function App() {
     return (
@@ -67,48 +68,5 @@ function TodoListCard() {
                 />
             ))}
         </React.Fragment>
-    );
-}
-
-function AddItemForm({ onNewItem }) {
-    const [newItem, setNewItem] = React.useState('');
-    const [submitting, setSubmitting] = React.useState(false);
-
-    const submitNewItem = (e) => {
-        e.preventDefault();
-        setSubmitting(true);
-        fetch('/items', {
-            method: 'POST',
-            body: JSON.stringify({ name: newItem }),
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then((r) => r.json())
-            .then((item) => {
-                onNewItem(item);
-                setSubmitting(false);
-                setNewItem('');
-            });
-    };
-
-    return (
-        <Form onSubmit={submitNewItem}>
-            <InputGroup className="mb-3">
-                <Form.Control
-                    value={newItem}
-                    onChange={(e) => setNewItem(e.target.value)}
-                    type="text"
-                    placeholder="New Item"
-                    aria-describedby="basic-addon1"
-                />
-                <Button
-                    type="submit"
-                    variant="success"
-                    disabled={submitting || !newItem.length}
-                    className={submitting ? 'disabled' : ''}
-                >
-                    {submitting ? 'Adding...' : 'Add Item'}
-                </Button>
-            </InputGroup>
-        </Form>
     );
 }
