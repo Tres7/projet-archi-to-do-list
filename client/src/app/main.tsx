e20fb6db4aaaa1d5b95eccc0c545d2d8284b5c2a
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-02-14 07:55:08 +0100

    style: update main.tsx to use styles.scss for styling

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './styles.css';
+import './styles.scss';
 
 import App from './App.tsx';
 
