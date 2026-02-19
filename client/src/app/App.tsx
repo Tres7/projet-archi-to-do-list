e9b27bb8a4fff81545f791c46519ab517163646f
Godwin <kpoglo.godwin10@gmail.com>
2026-02-20 00:04:44 +0100

    feat: add PrivateRoute component and add to App

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TodoPage from '../pages/TodoPage';
 import AuthPage from '../pages/AuthPage';
+import PrivateRoute from '../shared/ui/PrivateRoute';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<TodoPage />} />
+                <Route path="/" element={
+                    <PrivateRoute>
+                        <TodoPage />
+                    </PrivateRoute>
+                } />
                 <Route path="/auth" element={<AuthPage />} />
             </Routes>
         </BrowserRouter>
