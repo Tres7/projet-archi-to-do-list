fabebf79972a70c8f1f358022d33b6c566ad989f
Godwin <kpoglo.godwin10@gmail.com>
2026-02-20 00:01:45 +0100

    feat: add Header to TodoPage

--- client/src/pages/TodoPage.tsx
+++ client/src/pages/TodoPage.tsx
@@ -1,14 +1,18 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import TodoListCard from '../features/todo/ui/TodoListCard';
+import Header from '../shared/ui/Header';
 
 export default function TodoPage() {
     return (
-        <Container>
-            <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <TodoListCard />
-                </Col>
-            </Row>
-        </Container>
+        <>
+        <Header />
+            <Container className="mt-4">
+                <Row>
+                    <Col md={{ offset: 3, span: 6 }}>
+                        <TodoListCard />
+                    </Col>
+                </Row>
+            </Container>
+        </>    
     );
 }
