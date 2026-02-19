a86d22e42fa46bff2d017af631db621e1460718d
Godwin <kpoglo.godwin10@gmail.com>
2026-02-20 00:49:04 +0100

    feat: add user-api

---
+++ client/src/features/user/api/user-api.ts
@@ -0,0 +1,13 @@
+import { apiClient } from '../../../shared/api/apiClient';
+
+export const userApi = {
+    updateUsername: async (id: string, username: string): Promise<void> => {
+        await apiClient.patch(`/users/${id}/name`, { username });
+    },
+    changePassword: async (id: string, password: string): Promise<void> => {
+        await apiClient.patch(`/users/${id}/password`, { password });
+    },
+    deleteAccount: async (id: string): Promise<void> => {
+        await apiClient.delete(`/users/${id}`);
+    },
+};
