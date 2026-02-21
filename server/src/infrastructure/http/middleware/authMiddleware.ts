cc84b46095fa4c2f55f2969fb633ee9fa347b796
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-02-21 12:31:07 +0100

    fix: authMiddleware

--- server/src/infrastructure/http/middleware/authMiddleware.ts
+++ server/src/infrastructure/http/middleware/authMiddleware.ts
@@ -14,7 +14,7 @@ export function authMiddleware(
     try {
         const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({ error: 'Unauthorized' });
         }
 
         const decoded = jwt.verify(
