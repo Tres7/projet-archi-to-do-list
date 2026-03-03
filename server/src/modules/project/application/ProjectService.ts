158f38cb22ef4e1a27fc0ebb5e532ba10c3b8e1e
Godwin <kpoglo.godwin10@gmail.com>
2026-03-03 01:06:35 +0100

    feat(project module): add application layer

---
+++ server/src/modules/project/application/ProjectService.ts
@@ -0,0 +1,58 @@
+import { v4 as uuid } from 'uuid';
+import { Project } from '../domain/entities/Project.ts';
+import { NotFoundError } from '../../../common/errors/NotFoundError.ts';
+import { UnauthorizedError } from '../../../common/errors/UnauthorizedError.ts';
+import type { ProjectRepository } from '../domain/repositories/ProjectRepository.ts';
+
+export interface IProjectService {
+    createProject(name: string, description: string, ownerId: string): Promise<Project>;
+    closeProject(id: string, ownerId: string): Promise<void>;
+    deleteProject(id: string, ownerId: string): Promise<void>;
+    getAllProjects(ownerId: string): Promise<Project[]>;
+}
+
+export class ProjectService implements IProjectService {
+    constructor(
+        private readonly projectRepository: ProjectRepository    
+    ) {}
+
+    async createProject(name: string, description: string, ownerId: string): Promise<Project> {
+        const project = new Project(uuid(), name, description, 'opened', 0, [], ownerId);
+        await this.projectRepository.storeProject(project);
+        return project;
+    }
+
+
+    async closeProject(id: string, ownerId: string): Promise<void> {
+        const project = await this.projectRepository.getProject(id);
+
+        if (!project) {
+            throw new NotFoundError();
+        }
+
+        if (project.owner_id !== ownerId) {
+            throw new UnauthorizedError();
+        }
+
+        await this.projectRepository.updateProject(id, 'closed');
+        
+    }
+
+    async deleteProject(id: string, ownerId: string): Promise<void> {
+        const project = await this.projectRepository.getProject(id);
+
+        if (!project) {
+            throw new NotFoundError();
+        }
+
+        if (project.owner_id !== ownerId) {
+            throw new UnauthorizedError();
+        }
+        
+        await this.projectRepository.removeProject(id);
+    }
+
+    async getAllProjects(ownerId: string): Promise<Project[]> {
+        return this.projectRepository.getProjects(ownerId);
+    }
+}
