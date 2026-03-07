4980125dbee2d68bbdfe18f4c90115ed18da176e
Godwin <kpoglo.godwin10@gmail.com>
2026-03-07 23:44:22 +0100

    feat(project): update routing for project pages

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import TodoPage from '../pages/TodoPage';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AuthPage from '../pages/AuthPage';
 import PrivateRoute from '../shared/ui/PrivateRoute';
 import ProfilePage from '../pages/ProfilePage';
+import ProjectDetailPage from '../pages/ProjectDetailPage';
+import ProjectsPage from '../pages/ProjectsPage';
 
 export default function App() {
     return (
@@ -10,15 +11,30 @@ export default function App() {
             <Routes>
                 <Route path="/" element={
                     <PrivateRoute>
-                        <TodoPage />
+                        <Navigate to="/projects" />
                     </PrivateRoute>
                 } />
+
+                <Route path="/projects" element={
+                    <PrivateRoute>
+                        <ProjectsPage />
+                    </PrivateRoute>
+                } />
+
+                <Route path="/projects/:projectId" element={
+                    <PrivateRoute>
+                        <ProjectDetailPage />
+                    </PrivateRoute>
+                } />
+
                 <Route path="/profile" element={
                     <PrivateRoute>
                         <ProfilePage />
                     </PrivateRoute>
                 } />
+
                 <Route path="/auth" element={<AuthPage />} />
+
             </Routes>
         </BrowserRouter>
     );
