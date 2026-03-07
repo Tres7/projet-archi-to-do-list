c31e0b883cab433955af786904d9a4fe5dd1568c
Anton Harzhanau <anton.harzhanau@gmail.com>
2026-03-07 09:02:52 +0100

    refactor: update Project routes and controllers

--- server/src/modules/project/infrastructure/http/controllers/ProjectController.ts
+++ server/src/modules/project/infrastructure/http/controllers/ProjectController.ts
@@ -1,40 +1,42 @@
 import { NotFoundError } from '../../../../../../common/errors/NotFoundError.ts';
 import { UnauthorizedError } from '../../../../../../common/errors/UnauthorizedError.ts';
-import type { IProjectService } from '../../../application/ProjectService.ts';
 import type { Request, Response } from 'express';
+import type { ProjectService } from '../../../application/ProjectService.ts';
 
 export class ProjectController {
-    constructor(private readonly projectService: IProjectService) {}
+    constructor(private readonly projectService: ProjectService) {}
 
     getProjects = async (req: Request, res: Response) => {
         try {
             res.send(
-                await this.projectService.getAllProjects(
-                    req.currentUser.userId,
-                ),
+                await this.projectService.getProjects(req.currentUser.userId),
             );
         } catch (e) {
             res.status(500).json({ error: 'Failed to fetch projects' });
         }
     };
 
+    getProjectDetails = async (req: Request, res: Response) => {
+        try {
+            const result = await this.projectService.getProjectDetails(
+                String(req.params.projectId),
+                req.currentUser.userId,
+            );
+            res.send(result);
+        } catch (e) {
+            res.status(500).json({ error: 'Failed to fetch project details' });
+        }
+    };
+
     addProject = async (req: Request, res: Response) => {
         try {
-            const name = String(req.body?.name).trim();
-
-            const description = String(req.body?.description ?? '').trim();
-
-            if (!name) {
-                return res.status(400).json({ error: 'name is required' });
-            }
-
-            res.send(
-                await this.projectService.createProject(
-                    name,
-                    description,
-                    req.currentUser.userId,
-                ),
-            );
+            const result = await this.projectService.requestCreateProject({
+                ownerId: req.currentUser.userId,
+                ownerEmail: 'test@example.com',
+                name: String(req.body.name),
+                description: String(req.body.description ?? ''),
+            });
+            res.status(201).send(result);
         } catch (e) {
             res.status(500).json({ error: 'Failed to create project' });
         }
@@ -42,10 +44,11 @@ export class ProjectController {
 
     closeProject = async (req: Request, res: Response) => {
         try {
-            await this.projectService.closeProject(
-                String(req.params.id),
-                req.currentUser.userId,
-            );
+            await this.projectService.requestCloseProject({
+                projectId: String(req.params.projectId),
+                ownerId: req.currentUser.userId,
+                ownerEmail: 'test@example.com',
+            });
             res.sendStatus(200);
         } catch (e) {
             if (e instanceof NotFoundError) {
@@ -60,23 +63,88 @@ export class ProjectController {
         }
     };
 
-    deleteProject = async (req: Request, res: Response) => {
+    // deleteProject = async (req: Request, res: Response) => {
+    //     try {
+    //         await this.projectService.deleteProject(
+    //             String(req.params.projectId),
+    //             req.currentUser.userId,
+    //         );
+    //         res.sendStatus(200);
+    //     } catch (e) {
+    //         if (e instanceof NotFoundError) {
+    //             return res.status(404).json({ error: 'Project not found' });
+    //         }
+
+    //         if (e instanceof UnauthorizedError) {
+    //             return res.status(403).json({ error: 'Forbidden' });
+    //         }
+
+    //         res.status(500).json({ error: 'Failed to delete project' });
+    //     }
+    // };
+
+    createTask = async (req: Request, res: Response) => {
         try {
-            await this.projectService.deleteProject(
-                String(req.params.id),
-                req.currentUser.userId,
-            );
-            res.sendStatus(200);
+            const result = await this.projectService.requestCreateTask({
+                projectId: String(req.params.projectId),
+                userId: req.currentUser.userId,
+                userEmail: 'test@example.com',
+                name: String(req.body.name),
+                description: String(req.body.description ?? ''),
+            });
+            res.status(201).send(result);
         } catch (e) {
-            if (e instanceof NotFoundError) {
-                return res.status(404).json({ error: 'Project not found' });
+            res.status(500).json({ error: 'Failed to create task' });
+        }
+    };
+
+    toggleTaskStatus = async (req: Request, res: Response) => {
+        try {
+            const result = await this.projectService.requestToggleTaskStatus({
+                projectId: String(req.params.projectId),
+                taskId: String(req.params.taskId),
+                userId: req.currentUser.userId,
+                userEmail: 'test@example.com',
+            });
+
+            res.status(202).json(result);
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                return res
+                    .status(404)
+                    .json({ error: 'Project or Task not found' });
             }
 
-            if (e instanceof UnauthorizedError) {
+            if (error instanceof UnauthorizedError) {
                 return res.status(403).json({ error: 'Forbidden' });
             }
 
-            res.status(500).json({ error: 'Failed to delete project' });
+            res.status(500).json({ error: 'Failed to toggle task status' });
+        }
+    };
+
+    deleteTask = async (req: Request, res: Response) => {
+        try {
+            const result = await this.projectService.requestDeleteTask({
+                projectId: String(req.params.projectId),
+                taskId: String(req.params.taskId),
+                userId: req.currentUser.userId,
+                userEmail: 'test@example.com',
+            });
+
+            res.status(202).json(result);
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                return res
+                    .status(404)
+                    .json({ error: 'Project or Task not found' });
+            }
+
+            if (error instanceof UnauthorizedError) {
+                return res.status(403).json({ error: 'Forbidden' });
+            }
+
+            res.status(500).json({ error: 'Failed to delete task' });
         }
     };
 }
--- server/src/modules/project/infrastructure/http/routes/projectRoutes.ts
+++ server/src/modules/project/infrastructure/http/routes/projectRoutes.ts
@@ -5,9 +5,18 @@ export function projectRouter(controller: ProjectController): Router {
     const router = Router();
 
     router.get('/', controller.getProjects);
+    router.get('/:projectId/details', controller.getProjectDetails);
+
     router.post('/', controller.addProject);
-    router.patch('/:id/close', controller.closeProject);
-    router.delete('/:id', controller.deleteProject);
+    router.post('/:projectId/close', controller.closeProject);
+    // router.delete('/:id', controller.deleteProject);
+
+    router.post('/:projectId/tasks', controller.createTask);
+    router.patch(
+        '/:projectId/tasks/:taskId/toggle-status',
+        controller.toggleTaskStatus,
+    );
+    router.delete('/:projectId/tasks/:taskId', controller.deleteTask);
 
     return router;
 }
